8dc5e893e26adcfa5957220db5168e98d7bc5023
Melissa Holmes <meltar@gmail.com>
2014-10-27 23:05:59 -0400

    Add model level validations

--- app/models/halloweenpet.rb
+++ app/models/halloweenpet.rb
@@ -1,3 +1,33 @@
 class Halloweenpet < ActiveRecord::Base
 	mount_uploader :picture, PictureUploader
+
+	# Name must be provided
+	validates_presence_of :name
+
+	# Require a minimum length
+	validates :name, length: { minimum: 2 }
+
+	# Email address must be unique
+	# Uniqueness checks are case sensitive by default
+	validates :email, uniqueness: { case_sensitive: false }
+
+	# Check min and max length of description and return an error message
+	# if the input fails one of the rules
+	validates :description, length: {
+		minimum: 1,
+		maximum: 255,
+		too_short: "%{count} characters is the minimum allowed",
+		too_long: "%{count} characters is the maximum allowed" }
+
+	# Make sure the filename was provided
+	validates_presence_of :picture
+
+	# Ensure the age is 1 or 2 digits and an integer
+	validates :age, length: { in: 1..2 }
+
+
+	# These strings are case sensitive
+	validates :gender, inclusion: { in: ["male", "Male",
+																			 "female", "Female",
+																			 "unknown", "Unknown"]}
 end
